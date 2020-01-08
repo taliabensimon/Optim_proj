285bd5f3c338d56d87e84832eb0b72e2b1efb805
Roxnya <chernroni@gmail.com>
2020-01-08 18:34:45 +0200

    refactor

---
+++ tree.py
@@ -0,0 +1,21 @@
+import scipy.optimize.linprog as scilp
+import enum
+
+class LPResult(enum.Enum):
+    VAR_COEFF = 0,
+    FUNC_VAL = 1,
+    SLACKS_VAL = 2,
+    CON = 3, #The (nominally zero) residuals of the equality constraints, b_eq - A_eq @ x
+    SUCESS = 4,
+    STATUS = 5,
+    NUM_ITER = 6,
+    MSG = 7
+
+class Tree(object):
+    def lp_node_value(self, problem):
+        result = scilp(problem.func_coeff, problem.constraint_coeff, problem.constraint_bound, problem.var_bounds)
+        return result
+
+    def GetNext(self):
+        pass
+
