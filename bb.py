a8bc02d3771adba29b60fb00e9ac9c0f19c7daee
taliabs <taliabsimon@gmail.com>
2020-01-09 15:53:01 +0200

    update bb

--- bb.py
+++ bb.py
@@ -1,5 +1,23 @@
 from heapq import *
+from Tree import Tree
 
-class BranchAndBound(object):
+
+class MaxHeap(object):
+
+    def __init__(self):
+        self.priority_queue = []
+
+    def add(self,data):
+        for i in data:
+            item = (-i.val, i)
+            heappush(self.priority_queue, item)
+
+    def get_item(self):
+        if len(self.priority_queue) == 0:
+            return None
+        return heappop(self.priority_queue)[1]
+
+
+class BranchAndBound(Tree):
     def __init__(self):
         self.priority_queue = []
