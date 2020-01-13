bc9c3c0ed8c7b241a05928736fa7cf152cd318e2
Roxnya <chernroni@gmail.com>
2020-01-13 19:10:58 +0200

    add addition calculation for lp

--- tree.py
+++ tree.py
@@ -2,6 +2,7 @@ from scipy.optimize import linprog as scilp
 import enum
 from node import Node
 import copy
+import numpy as np
 
 class LPResult(enum.Enum):
     VAR_COEFF = 0,
@@ -22,6 +23,13 @@ class Tree(object):
 
     def lp_node_value(self, problem):
         result = scilp(problem.func_coeff,A_ub = problem.constraint_coeff,b_ub = problem.constraint_bound, bounds = problem.var_bounds)
+        return result
+
+    def get_lp_addition(self, var_vals, level, problem, ):
+        addition = np.dot(var_vals[:level], problem.original_func_coeff[:level])
+        result = self.lp_node_value(problem)
+        if result["success"]:
+            result["fun"] += addition
         return result
 
     def get_children(self,node):
