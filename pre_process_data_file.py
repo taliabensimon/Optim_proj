68fced98ce46c7e8c240452a1d033945533edd92
Roxnya <chernroni@gmail.com>
2020-01-26 00:22:47 +0200

    added new data preprocess

--- pre_process_data_file.py
+++ pre_process_data_file.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 from problem import  *
 import pickle
@@ -12,8 +14,8 @@ def mknap1():
             continue
         problem = problem.strip().split("\n ")
         num_vars, num_consts, opt_sol = np.array(problem[0].split(" "), dtype=float)
-        if opt_sol == 0:  # indicates that solution is unavailable
-            continue
+        # if opt_sol == 0:  # indicates that solution is unavailable
+        #     continue
 
         i = 1
         func_coeff = np.array(problem[i].split(" "), dtype=float) * -1
@@ -43,49 +45,74 @@ def mknap1():
         pickle.dump(problems, f)
 
 def mknapcb():
-    files = [f"mknapcb{i}.txt" for i in range(1, 10)]
+    # files = [f"mknapcb{i}.txt" for i in range(1, 10)]
+    files = []
+    path = f"{os.getcwd()}/data"
+    dirs = [f"{path}/gk"]
+    dirs += [f"{path}/sac94/{d}" for d in os.listdir(f"{path}/sac94") if "." not in d]
+    for d in dirs:
+        for f in os.listdir(d):
+            if "." in f[0]:
+                continue
+            files.append(f"{d}/{f}")
+
+    problems = []
     for f_id, file in enumerate(files):
-
-        problems = []
-        with open(file) as f:
-            content = f.readlines()
-        num_problems = int(content[0][:-1])
-        i=1
-        for _ in range(num_problems):
-            num_vars, num_consts, opt_sol = np.array(content[i][:-1].strip().split(" "), dtype=float)
-            i+=1
-            func_coeff = np.array(content[i][:-1].strip().split(" "), dtype=float) * -1
-            while len(func_coeff) < num_vars:
-                i += 1
-                func_coeff = np.concatenate((func_coeff, np.array(content[i][:-1].strip().split(" "), dtype=float) * -1))
+        if "gk" in file:
+            continue
+        print(file)
+        with open(file, "r") as f:
+            content = f.read().replace("\t"," ").split("\n")
+        #num_problems = #if #int(content[0][:-1])
+        i=0
+        num_vars, num_consts, opt_sol = np.array(content[i].strip().replace("  "," ").split(" "), dtype=float)
+        if opt_sol == 0:  # indicates that solution is unavailable
+            continue
+        # i+=1
+        func_coeff = []
+        # func_coeff = np.array(content[i][:-1].strip().replace("  ", " ").replace("\t"," ").split(" "), dtype=float) * -1
+        while len(func_coeff) < num_vars:
             i += 1
-            assert len(func_coeff) == num_vars
-            var_bound = [(0, 1) for f in range(len(func_coeff))]
+            for c in content[i].split(" "):
+                if c.isdigit():
+                    func_coeff.append(float(c))
+            # func_coeff = np.concatenate((func_coeff, np.array(content[i][:-1].strip().replace("  ", " ").replace("\t"," ").split(" "), dtype=float) * -1))
+        # i += 1
+        assert len(func_coeff) == num_vars
+        var_bound = [(0, 1) for f in range(len(func_coeff))]
 
-            const_coeffs = []
+        const_coeffs = []
 
-            j = i
-            for _ in range(i, i + int(num_consts)):
-                const_coeff = np.array(content[j][:-1].strip().split(" "), dtype=float)
+        j = i
+        for _ in range(i, i + int(num_consts)):
+            # const_coeff = np.array(content[j][:-1].strip().replace("  ", " ").replace("\t"," ").split(" "), dtype=float)
+            const_coeff = []
+            while len(const_coeff) < num_vars:
 
-                while len(const_coeff) < num_vars:
-                    j += 1
-                    const_coeff = np.concatenate((const_coeff, np.array(content[j][:-1].strip().split(" "), dtype=float)))
                 j += 1
-                const_coeffs.append(const_coeff)
-            assert len(const_coeffs) == int(num_consts)
+                for c in content[j].split(" "):
+                    if c.isdigit():
+                        const_coeff.append(float(c))
+                # const_coeff = np.concatenate((const_coeff, np.array(content[j][:-1].strip().replace("  ", " ").replace("\t"," ").split(" "), dtype=float)))
+            # j += 1
+            const_coeffs.append(const_coeff)
+        assert len(const_coeffs) == int(num_consts)
 
-            const_bound = np.array(content[j][:-1].strip().split(" "), dtype=float)
-            while len(const_bound) < len(const_coeffs):
-                j += 1
-                const_bound = np.concatenate((const_bound, np.array(content[j][:-1].strip().split(" "), dtype=float)))
+        const_bound = []
+        # const_bound = np.array(content[j][:-1].strip().replace("  ", " ").replace("\t"," ").split(" "), dtype=float)
+        while len(const_bound) < len(const_coeffs):
             j += 1
-            assert len(const_bound) == int(num_consts)
+            for c in content[j].split(" "):
+                if c.isdigit():
+                    const_bound.append(float(c))
+            # const_bound = np.concatenate((const_bound, np.array(content[j][:-1].strip().replace("  ", " ").replace("\t"," ").split(" "), dtype=float)))
+        j += 1
+        assert len(const_bound) == int(num_consts)
 
-            i = j
-            problems.append((Problem(OptType.MIN, func_coeff, const_coeffs, const_bound, var_bound, func_coeff), opt_sol))
-        with open(f"problems_mknapcb{f_id+1}_pickle", "wb") as f:
-            pickle.dump(problems, f)
+        i = j
+        problems.append((Problem(OptType.MIN, func_coeff, const_coeffs, const_bound, var_bound, func_coeff), opt_sol))
+    with open(f"problems_large_data_pickle", "wb") as f:
+        pickle.dump(problems, f)
 
 
 if __name__ == '__main__':
